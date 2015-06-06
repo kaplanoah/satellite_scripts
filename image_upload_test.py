d4329b89211b8f9bfe5ff7d035abac6351ef85ab
Noah Kaplan <noahkaplan4@gmail.com>
2015-06-06 12:32:28 -0700

    changed file name structure

--- image_upload_test.py
+++ image_upload_test.py
@@ -4,11 +4,12 @@ import time
 url   = 'http://localhost:3000/selfies.json'
 files = {'file': open('satellite_images/test.jpg', 'rb')}
 
-user_id     = 1
-selfie_name = 'image_from_satellite'
-timestamp   = int(time.time())
+user_id         = '28'
+earth_selfie_id = '119'
+selfie_name     = 'image_from_satellite'
+timestamp       = int(time.time())
 
-selfie_file_name = '_'.join([str(user_id), selfie_name, str(timestamp)]) + '.jpg'
+selfie_file_name = '_'.join([user_id, earth_selfie_id, selfie_name, str(timestamp)]) + '.jpg'
 
 r = requests.post(url, {'name': selfie_file_name}, files=files)
 
